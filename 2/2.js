30a9184eecdfa2c85ac5c1a0a0727a4309daca6f
ICreeper12 <54474482+ICreeper12@users.noreply.github.com>
2020-10-19 16:47:54 +0200

    Add files via upload

---
+++ 2/2.js
@@ -0,0 +1,12 @@
+function next() {
+window.location="3.html";
+}
+function nextt() {
+window.location="2.html";
+}
+function nexttt() {
+window.location="3.html";
+}
+function nextttt() {
+	window.location="1.html";
+}
